afb65d4190687a642dd70176a4efa27c2263f6c4
elshaniart <56869196+elshaniart@users.noreply.github.com>
2021-11-20 19:10:30 +0100

    some package updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-state = {
-    data: null
+  state = {
+    data: null,
   };
 
   componentDidMount() {
     this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
+      .then((res) => this.setState({ data: res.express }))
+      .catch((err) => console.log(err));
   }
-    // fetching the GET route from the Express server which matches the GET route from server.js
+  // fetching the GET route from the Express server which matches the GET route from server.js
   callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
+    const response = await fetch("/express_backend");
     const body = await response.json();
 
     if (response.status !== 200) {
-      throw Error(body.message) 
+      throw Error(body.message);
     }
     return body;
   };
@@ -26,7 +25,7 @@ state = {
   render() {
     return (
       <div className="App">
-      <header>
+        <header>
           <h1 className="App-title">Welcome to the NFT Project</h1>
         </header>
         <button>Go to next Pag</button>
